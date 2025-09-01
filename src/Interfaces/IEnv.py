6f4c3e338125a888b988302527dca1b6877d612a
UMAsouls <umaplay972@gmail.com>
2025-09-01 22:23:58 +0900

    [add] インターフェースの定義

---
+++ src/Interfaces/IEnv.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from torch import Tensor
+
+class IEnv(ABC):
+    @abstractmethod
+    def get_board_player1() -> Tensor:
+        pass
+    
+    @abstractmethod
+    def get_board_player2() -> Tensor:
+        pass
+    
+    @abstractmethod
+    def reset() -> None:
+        pass
+    
+    @abstractmethod
+    def step(action: int) -> tuple[Tensor, bool]:
+        pass
+    
+    @abstractmethod
+    def undo() -> bool:
+        pass
