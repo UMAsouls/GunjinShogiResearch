a63240f43c4c4710c84f91595c86896fa2062d88
UMAsouls <umaplay972@gmail.com>
2025-09-03 02:03:00 +0900

    [fix] IJudgeBoardのjudgeの返り値をEraseFragへ

--- src/GunjinShogi/Interfaces/IJudgeBoard.py
+++ src/GunjinShogi/Interfaces/IJudgeBoard.py
@@ -2,13 +2,14 @@ from abc import ABC, abstractmethod
 from torch import Tensor
 
 from GunjinShogi.Interfaces.IBoard import IBoard
+from GunjinShogi.const import EraseFrag
 
 
 
 class IJudgeBoard(IBoard):
     
     @abstractmethod
-    def judge(self, action: int, player: int) -> bool:
+    def judge(self, action: int, player: int) -> EraseFrag:
         pass
     
     @abstractmethod
