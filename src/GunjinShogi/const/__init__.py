206f34738b2588b544d8f781fd9b7bdad56f2854
UMAsouls <umaplay972@gmail.com>
2025-09-03 02:02:29 +0900

    [fix] Gunjinshogi.constのinit

--- src/GunjinShogi/const/__init__.py
+++ src/GunjinShogi/const/__init__.py
@@ -1,1 +1,4 @@
 from GunjinShogi.const.EraseFrag import EraseFrag
+from GunjinShogi.const.Piece import Piece
+from GunjinShogi.const.JudgeFrag import JudgeFrag
+from GunjinShogi.const.JudgeTable import JUDGE_TABLE
