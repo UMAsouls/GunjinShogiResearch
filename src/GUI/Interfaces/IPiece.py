adc46a87658a1a702454bd7ab383c31511103a89
UMAsouls <umaplay972@gmail.com>
2025-09-08 22:04:05 +0900

    [add] IPiece

---
+++ src/GUI/Interfaces/IPiece.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+import pygame as pg
+
+class IPiece(ABC):
+    @abstractmethod
+    def set_location(self, pos:tuple[int,int]) -> None:
+        pass
+    
+    @abstractmethod
+    def draw(self, surface: pg.Surface) -> None:
+        pass
+    
+    @abstractmethod
+    def chg_appear(self) -> None:
+        pass
---
+++ src/GUI/Interfaces/__init__.py
@@ -0,0 +1,1 @@
+from GUI.Interfaces.IPiece import IPiece
