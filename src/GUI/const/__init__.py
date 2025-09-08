32031438568040cae72bb45dc775af4bf5ecf726
UMAsouls <umaplay972@gmail.com>
2025-09-09 04:08:43 +0900

    [fix] 細かい修正

--- src/GUI/const/__init__.py
+++ src/GUI/const/__init__.py
@@ -1,2 +1,2 @@
 from src.GUI.const.Sizes import WINDOW_SIZE,BOARD_SIZE,MASS_SIZE,PIECE_POINTS,BOARD_SHAPE
-from src.GUI.const.Colors import PIECE_COLOR
+from src.GUI.const.Colors import PIECE_COLOR, BOARD_COLOR
