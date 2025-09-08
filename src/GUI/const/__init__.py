a85fb6d32eddd12130e01b15ac2af7d641178025
UMAsouls <umaplay972@gmail.com>
2025-09-09 03:39:17 +0900

    [add] BOARD_SHAPE のimport

--- src/GUI/const/__init__.py
+++ src/GUI/const/__init__.py
@@ -1,2 +1,2 @@
-from src.GUI.const.Sizes import WINDOW_SIZE,BOARD_SIZE,MASS_SIZE,PIECE_POINTS
+from src.GUI.const.Sizes import WINDOW_SIZE,BOARD_SIZE,MASS_SIZE,PIECE_POINTS,BOARD_SHAPE
 from src.GUI.const.Colors import PIECE_COLOR
