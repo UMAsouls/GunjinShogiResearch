e8da8a11c7608030021b8b79df0a858daa524491
UMAsouls <umaplay972@gmail.com>
2025-09-16 11:49:56 +0900

    [add] Piece用のConfig

---
+++ src/const/PieceConfig.py
@@ -0,0 +1,31 @@
+from src.const.Piece import Piece
+
+piece_dict = [
+    Piece.General,
+    Piece.LieutenantGeneral,
+    Piece.MajorGeneral,
+    Piece.Colonel,
+    Piece.LieutenantColonel,
+    Piece.Major,
+    Piece.Captain,
+    Piece.Captain,
+    Piece.FirstLieunant,
+    Piece.FirstLieunant,
+    Piece.SecondLieunant,
+    Piece.SecondLieunant,
+    Piece.Plane,
+    Piece.Plane,
+    Piece.Tank,
+    Piece.Tank,
+    Piece.Cavalry,
+    Piece.Engineer,
+    Piece.Engineer,
+    Piece.Spy,
+    Piece.LandMine,
+    Piece.LandMine,
+    Piece.Frag
+]
+
+PIECE_LIMIT = len(piece_dict)
+
+PIECE_DICT = [int(i) for i in piece_dict]
--- src/const/__init__.py
+++ src/const/__init__.py
@@ -1,3 +1,5 @@
 from src.const.Piece import Piece,PIECE_KINDS
 
 from src.const.BoardConfig import BOARD_SHAPE,BOARD_SHAPE_INT,ENTRY_HEIGHT,ENTRY_POS,GOAL_POS
+
+from src.const.PieceConfig import PIECE_LIMIT,PIECE_DICT
