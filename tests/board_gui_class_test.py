41946421693854d0df9b98d93afa6a43595a66fe
UMAsouls <umaplay972@gmail.com>
2025-09-17 11:19:44 +0900

    [fix] tensor_boardの座標ずれ直し

--- tests/board_gui_class_test.py
+++ tests/board_gui_class_test.py
@@ -27,22 +27,28 @@ def make_int_board(pieces1: torch.Tensor, pieces2: torch.Tensor) -> tuple[list[i
         while x < BOARD_SHAPE[0]: 
            board[y][x] = PIECE_DICT[pieces1[i].item()]
            i += 1
-           x += 1
            if(x in GOAL_POS and y == BOARD_SHAPE[1]-1): x += len(GOAL_POS)-1 
+           x += 1
            
     return board
 
-
+#tensorのboard作成関数
 def make_tensor_board(pieces: torch.Tensor) -> torch.Tensor:
     tensor_board = torch.full((BOARD_SHAPE_INT,), int(Piece.Enemy), dtype=torch.int32)
     
+    #entryとwallの部分入力
     wall_area = torch.arange(start=BOARD_SHAPE[0]*ENTRY_HEIGHT,end=BOARD_SHAPE[0]*(ENTRY_HEIGHT+1))
     tensor_board[wall_area] = int(Piece.Wall)
     entry = torch.as_tensor([BOARD_SHAPE[0]*ENTRY_HEIGHT+i for i in ENTRY_POS])
     tensor_board[entry] = int(Piece.Entry)
     
-    area = torch.arange(start=BOARD_SHAPE_INT-pieces.shape[0], end=BOARD_SHAPE_INT)
-    tensor_board[area] = PIECE_TENSOR_DICT[pieces]
+    #司令部の空白部分作成
+    space_pos = pieces.shape[0] - BOARD_SHAPE[0] + GOAL_POS[-1]
+    real_pieces = torch.cat((pieces[:space_pos],torch.tensor([int(Piece.Space)]),pieces[space_pos:]))
+    
+    #tensorの後半部分に代入
+    area = torch.arange(start=BOARD_SHAPE_INT-real_pieces.shape[0], end=BOARD_SHAPE_INT)
+    tensor_board[area] = PIECE_TENSOR_DICT[real_pieces]
     
     return tensor_board
     
