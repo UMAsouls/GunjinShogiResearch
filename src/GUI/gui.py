a246efa700b17c9a7d08ceb577d58efb8ad7af94
UMAsouls <umaplay972@gmail.com>
2025-09-18 20:01:42 +0900

    [fix] player2のpos変換を修正

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -14,7 +14,7 @@ def make_reflect_pos(pos:tuple[int,int]) -> tuple[int,int]:
     return (BOARD_SHAPE[0] - pos[0], BOARD_SHAPE[1] - pos[1])
 
 def make_reflect_pos_int(pos_int:int) -> int:
-    return BOARD_SHAPE_INT - pos_int
+    return BOARD_SHAPE_INT - (pos_int+1)
 
 class GUI:
     def __init__(self, boardgui: IBoardGUI, env: IEnv):
