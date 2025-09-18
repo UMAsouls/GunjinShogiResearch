e32e5ee808a884ef6097b240c8c892db903f28ef
UMAsouls <umaplay972@gmail.com>
2025-09-19 02:12:42 +0900

    [add] RandomAgent

---
+++ src/Agent/random_agent.py
@@ -0,0 +1,19 @@
+from src.Interfaces import IAgent, IEnv
+from src.const import PIECE_LIMIT
+
+import torch
+
+class RandomAgent(IAgent):
+    def __init__(self):
+        pass
+    
+    def get_action(self, env: IEnv):
+        legals = env.legal_move()
+        idx = torch.multinomial(torch.ones(legals.shape), num_samples=1, replacement=True)
+        return legals[idx]
+    
+    def get_first_board(self):
+        pieces = torch.arange(PIECE_LIMIT)
+        rand = torch.randperm(pieces.shape[0])
+        return pieces[rand]
+        
