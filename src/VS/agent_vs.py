97b8b0022561a479de0599793febfb4325c8a0f4
UMAsouls <umaplay972@gmail.com>
2025-09-19 12:05:24 +0900

    [add] Agent_VSにlog save機能

--- src/VS/agent_vs.py
+++ src/VS/agent_vs.py
@@ -2,8 +2,9 @@ from src.Interfaces import IAgent, IEnv
 
 from src.common import make_tensor_board, Player
 
+import numpy as np
 
-def Agent_VS(agent1: IAgent, agent2: IAgent, env: IEnv) -> int:
+def Agent_VS(agent1: IAgent, agent2: IAgent, env: IEnv, log_path:str = "") -> int:
     pieces1 = agent1.get_first_board()
     pieces2 = agent2.get_first_board()
     
@@ -14,12 +15,18 @@ def Agent_VS(agent1: IAgent, agent2: IAgent, env: IEnv) -> int:
     
     done = False
     agents = (agent1, agent2)
+    logs = []
     while not done:
         player = agents[env.get_current_player() - 1]
         
         action = player.get_action(env)
         
         _,log,done = env.step(action)
+        logs.append([log.action, log.player, log.erase, log.bef, log.aft])
+        
+    log_array = np.array(logs)
+    
+    if(log_path): np.save(log_path, log_array)
         
     if(env.get_current_player() == Player.PLAYER2): return 1
     elif(env.get_current_player() == Player.PLAYER1): return 2
