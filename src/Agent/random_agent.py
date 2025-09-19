c3fb3a201ad2e6edc8b41693b8798f8f2c7c02ed
UMAsouls <umaplay972@gmail.com>
2025-09-19 12:04:45 +0900

    [fix] random agentのactionをint化

--- src/Agent/random_agent.py
+++ src/Agent/random_agent.py
@@ -11,7 +11,7 @@ class RandomAgent(IAgent):
         legals = env.legal_move()
         idx = torch.multinomial(torch.ones(legals.shape), num_samples=1, replacement=True)
         action = legals[idx]
-        return action
+        return action.item()
     
     def get_first_board(self):
         pieces = torch.arange(PIECE_LIMIT)
