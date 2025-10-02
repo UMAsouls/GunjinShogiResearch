701c18451dd66d80d08b40e8805aa0517bd846f3
UMAsouls <umaplay972@gmail.com>
2025-10-03 06:25:26 +0900

    [add] Boardのヘッダー

---
+++ cpp/Board/Board.h
@@ -0,0 +1,33 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+#include "common/Piece.h"
+#include "common/Config.h"
+
+class Board {
+private:
+    std::vector<std::vector<Piece>> verticalBoard; // Example board representation
+    std::vector<std::vector<Piece>> horizontalBoard; // Example board representation
+
+    Config config;
+
+    void set(int x, int y, Piece piece);
+
+public:
+    Board(Config config) : config(config) {};
+    ~Board();
+
+    Board(std::vector<int> pieces, Config config);
+    void reset();
+
+    void erase(int x, int y);
+    void move(int fromX, int fromY, int toX, int toY);
+
+    Piece get(int x, int y) const;
+
+    std::vector<std::vector<Piece>> getCrossRange(int x, int y) const;
+
+};
+
+#endif
