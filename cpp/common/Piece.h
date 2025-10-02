53b9e171490d534b60ccc2c385b59d9292da6fc0
UMAsouls <umaplay972@gmail.com>
2025-10-03 05:18:26 +0900

    [add] Pieceのenum

---
+++ cpp/common/Piece.h
@@ -0,0 +1,29 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+enum Piece {
+    Space,
+    Enemy,
+    
+    Entry,
+    Wall,
+
+    General,
+    LieutenantGeneral,
+    MajorGeneral,
+    Colonel,
+    LieutenantColonel,
+    Major,
+    Captain,
+    FirstLieutenant,
+    SecondLieutenant,
+    Plane,
+    Tank,
+    Cavalry,
+    Engineer,
+    Spy,
+    LandMine,
+    Frag,
+};
+
+#endif // PIECE_H
