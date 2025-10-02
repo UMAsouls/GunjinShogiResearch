cdc457a0057d5cee9d5c07b4790616f3ce5591e1
UMAsouls <umaplay972@gmail.com>
2025-10-03 08:35:23 +0900

    [add] Boardのmoveとget_cross_range

--- cpp/Board/Board.cpp
+++ cpp/Board/Board.cpp
@@ -11,7 +11,23 @@ void Board::set(int x, int y, Piece piece) {
     horizontalBoard[y][x] = piece;
 }
 
-Board::Board(std::vector<int> pieces, Config config) : config(config) {
+Board::Board(std::vector<int> pieces, const Config& config) : config(config) {
+    first_pieces = pieces;
+    reset();
+
+}
+
+void Board::erase(int x, int y) {
+    set(x, y, Piece::Space);
+}
+
+void Board::move(int fromX, int fromY, int toX, int toY) {
+    Piece piece = get(fromX, fromY);
+    erase(fromX, fromY);
+    set(toX, toY, piece);
+}
+
+void Board::reset() {
     int entry_height = config.getEntryHeight();
     std::vector<int> entry_pos = config.getEntryPos();
     std::sort(entry_pos.begin(), entry_pos.end());
@@ -52,17 +68,24 @@ Board::Board(std::vector<int> pieces, Config config) : config(config) {
     int idx = 0;
     for (int i = entry_height + 1; i < shape_y; i++) {
         for (int j = 0; j < shape_x; j++) {
-            set(j, i, config.getPiece(pieces[idx++]));
+            set(j, i, config.getPiece(first_pieces[idx++]));
 
             if(j == f_goal && i == goal_height) {
                 j += goal_pos.size() - 1;
             }
         }
     }
-
 }
 
-void Board::erase(int x, int y) {
-    set(x, y, Piece::Space);
-}
+std::vector<std::vector<Piece>> Board::getCrossRange(int x, int y) const {
+    std::vector<std::vector<Piece>> ranges(4);
 
+    ranges[0] = std::vector<Piece>(horizontalBoard[y].begin() + x + 1, horizontalBoard[y].end());
+    ranges[1] = std::vector<Piece>(horizontalBoard[y].begin(), horizontalBoard[y].begin() + x);
+    std::reverse(ranges[1].begin(), ranges[1].end());
+    ranges[2] = std::vector<Piece>(verticalBoard[x].begin() + y + 1, verticalBoard[x].end());
+    ranges[3] = std::vector<Piece>(verticalBoard[x].begin(), verticalBoard[x].begin() + y);
+    std::reverse(ranges[3].begin(), ranges[3].end());
+
+    return ranges;
+}
--- cpp/Board/Board.h
+++ cpp/Board/Board.h
@@ -10,21 +10,23 @@ private:
     std::vector<std::vector<Piece>> verticalBoard; // Example board representation
     std::vector<std::vector<Piece>> horizontalBoard; // Example board representation
 
-    Config config;
+    const Config& config;
+
+    std::vector<int> first_pieces;
 
     void set(int x, int y, Piece piece);
 
 public:
-    Board(Config config) : config(config) {};
+    Board(const Config& config) : config(config) {};
     ~Board();
 
-    Board(std::vector<int> pieces, Config config);
+    Board(std::vector<int> pieces, const Config& config);
     void reset();
 
     void erase(int x, int y);
     void move(int fromX, int fromY, int toX, int toY);
 
-    Piece get(int x, int y) const;
+    Piece get(int x, int y) const { return verticalBoard[x][y]; }
 
     std::vector<std::vector<Piece>> getCrossRange(int x, int y) const;
 
