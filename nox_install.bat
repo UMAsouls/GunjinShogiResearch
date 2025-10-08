9741636dd0a85efc4d8a7612dcd568ae8b70438c
UMAsouls <umaplay972@gmail.com>
2025-10-08 22:11:55 +0900

    [add] install用batファイル

---
+++ nox_install.bat
@@ -0,0 +1,8 @@
+echo GunjinShogiCore Debug install
+conan install cpp/GunjinShogiCore -s build_type=Debug
+echo GunjinShogiCore Release install
+conan install cpp/GunjinShogiCore -s build_type=Release
+echo GunjinShogiResearch Debug install
+conan install . -s build_type=Debug
+echo GunjinShogiResearch Release install
+conan install . -s build_type=Release
