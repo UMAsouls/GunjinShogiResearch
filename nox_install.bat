c909f72584eb8bad6d9fe22bb647a70bd3634ac3
UMAsouls <umaplay972@gmail.com>
2025-10-08 22:41:03 +0900

    [add] editable addをマクロに追加

--- nox_install.bat
+++ nox_install.bat
@@ -1,3 +1,5 @@
+echo editable add GunjinShogiCore
+conan editable add cpp/GunjinShogiCore
 echo GunjinShogiCore Debug install
 conan install cpp/GunjinShogiCore -s build_type=Debug
 echo GunjinShogiCore Release install
