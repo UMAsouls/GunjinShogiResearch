1f5a5cb056cebc65ce8283ed61f640e35dfc740a
UMAsouls <umaplay972@gmail.com>
2025-11-11 02:42:30 +0900

    [rm] まだいらないものあったので

--- conan_install.bat
+++
@@ -1,4 +0,0 @@
-echo GunjinShogiResearch Debug install
-conan install . -s build_type=Debug --build=missing
-echo GunjinShogiResearch Release install
-conan install . -s build_type=Release --build=missing
