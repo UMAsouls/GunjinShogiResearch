049171c310a5784a790cec7b05eef1dacd2931cc
UMAsouls <umaplay972@gmail.com>
2025-12-14 17:08:13 +0900

    [fix] Coreの更新

--- GunjinShogiCore.pyi
+++ GunjinShogiCore.pyi
@@ -6,13 +6,22 @@ import enum
 import numpy
 import numpy.typing
 import typing
-__all__: list[str] = ['AFT', 'BEF', 'BOTH', 'BattleEndFrag', 'CONTINUE', 'EraseFrag', 'JudgeBoard', 'JudgeFrag', 'LOSE', 'MakeJudgeBoard', 'PIECE_DRAW', 'PIECE_LOSE', 'PIECE_WIN', 'PLAYER_ONE', 'PLAYER_TWO', 'Player', 'WIN']
+__all__: list[str] = ['AFT', 'BEF', 'BOTH', 'BattleEndFrag', 'CONTINUE', 'Config', 'DEPLOY_END', 'DRAW', 'EraseFrag', 'JudgeBoard', 'JudgeFrag', 'LOSE', 'MakeJudgeBoard', 'PIECE_DRAW', 'PIECE_LOSE', 'PIECE_WIN', 'PLAYER_ONE', 'PLAYER_TWO', 'Player', 'WIN']
 class BattleEndFrag(enum.Enum):
     CONTINUE: typing.ClassVar[BattleEndFrag]  # value = <BattleEndFrag.CONTINUE: 2>
+    DEPLOY_END: typing.ClassVar[BattleEndFrag]  # value = <BattleEndFrag.DEPLOY_END: 4>
+    DRAW: typing.ClassVar[BattleEndFrag]  # value = <BattleEndFrag.DRAW: 3>
     LOSE: typing.ClassVar[BattleEndFrag]  # value = <BattleEndFrag.LOSE: 1>
     WIN: typing.ClassVar[BattleEndFrag]  # value = <BattleEndFrag.WIN: 0>
-    DRAW: typing.ClassVar[BattleEndFrag] 
-    DEPLOY_END: typing.ClassVar[BattleEndFrag] 
+class Config:
+    def getBoardShape(self) -> tuple[int, int]:
+        ...
+    def getEntryHeight(self) -> int:
+        ...
+    def getGoalHeight(self) -> int:
+        ...
+    def loadFromJson(self, arg0: str) -> bool:
+        ...
 class EraseFrag(enum.Enum):
     AFT: typing.ClassVar[EraseFrag]  # value = <EraseFrag.AFT: 1>
     BEF: typing.ClassVar[EraseFrag]  # value = <EraseFrag.BEF: 0>
@@ -20,9 +29,17 @@ class EraseFrag(enum.Enum):
 class JudgeBoard:
     def erase(self, arg0: typing.SupportsInt, arg1: typing.SupportsInt, arg2: Player) -> None:
         ...
+    def get(self, arg0: typing.SupportsInt, arg1: typing.SupportsInt, arg2: Player) -> int:
+        ...
+    def getConfig(self) -> Config:
+        ...
+    def getDefinedBoard(self, arg0: typing.Annotated[numpy.typing.ArrayLike, numpy.int32], arg1: Player) -> JudgeBoard:
+        ...
     def getJudge(self, arg0: typing.SupportsInt, arg1: Player) -> JudgeFrag:
         ...
     def getLegalMove(self, arg0: Player) -> numpy.typing.NDArray[numpy.int32]:
+        ...
+    def get_int_board(self, arg0: Player) -> numpy.typing.NDArray[numpy.int32]:
         ...
     def isWin(self, arg0: Player) -> BattleEndFrag:
         ...
@@ -30,15 +47,9 @@ class JudgeBoard:
         ...
     def reset(self) -> None:
         ...
+    def setBoard(self, arg0: typing.Annotated[numpy.typing.ArrayLike, numpy.int32], arg1: typing.Annotated[numpy.typing.ArrayLike, numpy.int32]) -> None:
+        ...
     def step(self, arg0: typing.SupportsInt, arg1: Player, arg2: EraseFrag) -> BattleEndFrag:
-        ...
-    def get(self, x: typing.SupportsInt, y: typing.SupportsInt, p: Player) -> int:
-        ... 
-    def getDefinedBoard(self, pieces:numpy.typing.NDArray[numpy.int32], player: Player) -> "JudgeBoard":
-        ...
-    def get_int_board(self, player: Player) -> numpy.typing.NDArray[numpy.int32]:
-        ...
-    def setBoard(self, pieces1:numpy.typing.NDArray[numpy.int32], pieces2:numpy.typing.NDArray[numpy.int32]) -> None:
         ...
 class JudgeFrag(enum.Enum):
     PIECE_DRAW: typing.ClassVar[JudgeFrag]  # value = <JudgeFrag.PIECE_DRAW: 2>
@@ -47,7 +58,8 @@ class JudgeFrag(enum.Enum):
 class Player(enum.Enum):
     PLAYER_ONE: typing.ClassVar[Player]  # value = <Player.PLAYER_ONE: 0>
     PLAYER_TWO: typing.ClassVar[Player]  # value = <Player.PLAYER_TWO: 1>
-def MakeJudgeBoard(arg2: str) -> JudgeBoard:
+def MakeJudgeBoard(arg0: str) -> JudgeBoard:
     """
     Make JudgeBoard function
     """
+
